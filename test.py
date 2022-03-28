ece8bb1493449b10792ad5d5cf6a8d5698239c54
guoyankai <392759421@qq.com>
2022-03-28 18:38:31 +0800

    2022年3月28号:开辟新得人脸关键点识别项目

---
+++ test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/28 18:36
+# @Author  : guoyankai
+# @Email   : 392759421@qq.com
+# @File    : test.py.py
+# @software: PyCharm
+
+def test(test_loader):
+    return 0
+
+
+if __name__ =="__main__":
+    print(test(1))
---
+++ train.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/28 18:35
+# @Author  : guoyankai
+# @Email   : 392759421@qq.com
+# @File    : train.py.py
+# @software: PyCharm
+
+def train(train_loader):
+    return 0
+
+
+
+if  __name__ == "__main__":
+
+    print(train(1))

5d73f605cc84270e381eb30df3fc668c196f89b1
guoyankai <392759421@qq.com>
2022-03-30 18:37:03 +0800

    2022年3月30号:模型初始化处理

---
+++ models/weights_init.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/30 18:00
+# @Author  : guoyankai
+# @Email   : 392759421@qq.com
+# @File    : weights_init.py
+# @software: PyCharm
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2022/3/9 12:03
+# @Author  : guoyankai
+# @Email   : 392759421@qq.com
+# @File    : weights_init.py
+# @software: PyCharm
+
+import torch
+import torch.nn as nn
+from torch.nn import init
+
+
+def weights_init_normal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('Linear') != -1:
+        init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_xavier(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.xavier_normal_(m.weight.data, gain=1)
+    elif classname.find('Linear') != -1:
+        init.xavier_normal_(m.weight.data, gain=1)
+    elif classname.find('BatchNorm') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_kaiming(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('Linear') != -1:
+        init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+    elif classname.find('BatchNorm') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
+
+
+def weights_init_orthogonal(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        init.orthogonal_(m.weight.data, gain=1)
+    elif classname.find('Linear') != -1:
+        init.orthogonal_(m.weight.data, gain=1)
+    elif classname.find('BatchNorm') != -1:
+        init.normal_(m.weight.data, 1.0, 0.02)
+        init.constant_(m.bias.data, 0.0)
+
+
+def init_weights(net, init_type='normal'):
+    if init_type == 'normal':
+        net.apply(weights_init_normal)
+    elif init_type == 'xavier':
+        net.apply(weights_init_xavier)
+    elif init_type == 'kaiming':
+        net.apply(weights_init_kaiming)
+    elif init_type == 'orthogonal':
+        net.apply(weights_init_orthogonal)
+    else:
+        raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+
+
+#-----------------以前的初始化参数(已放弃使用)----------------------------------
+def init_model(net):
+    if isinstance(net, nn.Conv2d):
+        nn.init.kaiming_normal_(net.weight.data,0.25)
+        nn.init.constant_(net.bias.data,0.1)
+    elif isinstance(net,nn.BatchNorm2d):
+        nn.init.normal_(net.weight.data, 1.0, 0.02)
+        nn.init.constant_(net.bias.data, 0.1)
